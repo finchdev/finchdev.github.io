df77407803344ef5f85b835cb4ae174485efa22c
finch <44198957+finchdev@users.noreply.github.com>
2019-08-02 13:47:35 +0300

    Add files via upload

--- uxkit/js/main.js
+++ uxkit/js/main.js
@@ -9,5 +9,35 @@ $(document).ready(function () {
         });
     };
 
+    var fileUpload = function () {
+        $(".file-upload input[type=file]").change(function(){
+            var filename = $(this).val().replace(/.*\\/, "");
+            $(this).closest('.file-upload').find('.file-upload__text').html(filename);
+        });
+    };
+
+    var rangeSlider = function () {
+        var slider = document.getElementById("slider");
+
+        slider.oninput = function() {
+            $('.fill').css('width', this.value + '%');
+        }
+    };
+
+    var scrollTo = function () {
+        $("a.scrollto").click(function() {
+            var elementClick = $(this).attr("href")
+            var destination = $(elementClick).offset().top;
+            jQuery("html:not(:animated),body:not(:animated)").animate({
+                scrollTop: destination
+            }, 800);
+            return false;
+        });
+    };
+
+    scrollTo();
     tabs();
+    fileUpload();
+    rangeSlider();
+
 });
