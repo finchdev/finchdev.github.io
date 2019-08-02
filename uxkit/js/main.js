39c336a9b2828d5b1c31ebbbee3963768f3c19c1
finch <44198957+finchdev@users.noreply.github.com>
2019-08-02 11:04:28 +0300

    Add files via upload

--- uxkit/js/main.js
+++ uxkit/js/main.js
@@ -3,8 +3,9 @@ $(document).ready(function () {
     var tabs = function () {
         $('.tab-item').click(function() {
             var tabName = $(this).attr('show-tab');
+            $('.enlight-body .tab .enlight-body .tab').removeClass('tab--active');
             $(this).addClass('tab-item--active').siblings().removeClass('tab-item--active');
-            $('.enlight-body .' + tabName).addClass('tab--active').siblings().removeClass('tab--active');
+            $('.enlight-body .' + tabName).addClass('tab--active').siblings().removeClass('tab--active').find().removeClass('tab--active');
         });
     };
 
