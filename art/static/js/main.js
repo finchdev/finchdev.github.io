cf69d31036a211f92bf4b63d3e236acaaa2b08b9
finch <44198957+finchdev@users.noreply.github.com>
2019-11-16 16:48:23 +0200

    Add files via upload

--- art/static/js/main.js
+++ art/static/js/main.js
@@ -8,3 +8,42 @@ $('.hero-slider').slick({
     dots: true,
     infinite: true
 });
+
+$('.slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    centerMode: true,
+    variableWidth: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    prevArrow: document.querySelector('.prev'),
+    nextArrow: document.querySelector('.next'),
+    autoplaySpeed: 3000,
+    responsive: [
+        {
+            breakpoint: 1368,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 1034,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 769,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
